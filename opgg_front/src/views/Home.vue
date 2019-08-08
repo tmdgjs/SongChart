a6f40e737df1a8504e725908a16289efd76c41db
tmdgjs <xfirex1115@naver.com>
2019-08-09 05:00:25 +0900

    LOLWT -v1

---
+++ opgg_front/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+  
+    <Main />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Main from '@/components/Main.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style lang="scss">
+  
+</style>
---
+++ opgg_server/src/main/java/kr/hs/hansmari/opgg_server/OpggServerApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.hansmari.opgg_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpggServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpggServerApplication.class, args);
+    }
+
+}
